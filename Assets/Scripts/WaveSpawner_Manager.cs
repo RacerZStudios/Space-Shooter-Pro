e2434e4077bf776cff2d340807ea6a89a696e5a5
ZacB <zac.bogner@gmail.com>
2024-03-08 14:40:58 -0800

    wave system imp

---
+++ Assets/Scripts/WaveSpawner_Manager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WaveSpawner_Manager : MonoBehaviour
+{
+    [SerializeField] private GameObject ePrefab;
+    [SerializeField] private TextMeshProUGUI wave1;
+    private bool isWave1; 
+
+    private void Start()
+    {
+            
+    }
+}
